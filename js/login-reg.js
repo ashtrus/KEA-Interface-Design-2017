15c90f0d9ddbaee20d4c81faf26ed6994bb1ab46
LogicAppSourceIO <logicappsource@gmail.com>
2017-05-01 14:50:56 +0200

    Simple responsive login/sginup modal(flex)

---
+++ js/login-reg.js
@@ -0,0 +1,18 @@
+  $(document).ready(function() {
+        $('.modal').show();
+        $('.register').hide();
+    })
+
+    $('#login-reg-btn').on('click' , function () {
+        $('.modal').show();
+    })
+
+    $('#savebtn').on('click', function() {
+        $('.modal').hide();
+    })
+
+
+    $('#signUpbtn').on('click', function() {
+        $('.login').hide();
+        $('.register').show();
+    })
